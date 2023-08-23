3374e9e9f91e9a2fbb20b405c42c30c6fe6a2c2a
Jorge Meneses Saco <jorgejhms@gmail.com>
2023-08-23 11:10:41 -0500

    config inicial

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,23 @@
+return {
+  -- Temas de colores
+  { "navarasu/onedark.nvim", opts = { style = "darker" } },
+  { "EdenEast/nightfox.nvim" },
+  { "morhetz/gruvbox" },
+  { "NLKNguyen/papercolor-theme" },
+  { "sainnhe/edge" },
+  { "Mofiqul/dracula.nvim" },
+  { "oxfist/night-owl.nvim" },
+  { "rafamadriz/neon" },
+  { "sainnhe/sonokai" },
+  { "nvimdev/zephyr-nvim" },
+  { "tanvirtin/monokai.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  { "nyngwang/nvimgelion" },
+  { "mhartington/oceanic-next" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}
