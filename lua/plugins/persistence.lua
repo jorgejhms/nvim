3ed14fcbcd3ec620c3d9260f20e678361deafd38
Jorge Meneses <jorgejhms@gmail.com>
2024-03-26 16:04:25 -0500

    Agregando persistence

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,11 @@
+-- Lua
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    -- add any custom options here
+  },
+  keys = {
+   { "<leader>ql", ":lua require('persistence').load({ last = true })<cr>" }
+  }
+}
