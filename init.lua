ad7df75ef3029c54ddbcfc578191ff7efe5d175a
Jorge Meneses <jorgejhms@gmail.com>
2024-04-04 21:16:10 -0500

    Agregando config para neovide

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd("source $HOME/.config/nvim/config/nvim-r.vim")
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h13" -- text below applies for VimScript
+end
