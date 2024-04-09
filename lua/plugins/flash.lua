842eadd45d1cabc42a21f17b4bfc1f50b698ba30
Jorge Meneses <jorgejhms@gmail.com>
2024-04-09 09:39:05 -0500

    agregando flash

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,16 @@
+-- `flash.nvim` Permite navegar por los resultados de una búsqueda de forma visual.
+-- Revisar: h: :h flash.nvim
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- Selección de texto usando nodos de Treesitter
+    -- selcción incremental usando ";" y ","
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+}
