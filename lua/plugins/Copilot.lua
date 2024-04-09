05dd12314539a8ac50622b0be6c48bbe6299849c
Jorge Meneses <jorgejhms@gmail.com>
2024-04-09 09:39:00 -0500

    actualizando copilot

--- lua/plugins/Copilot.lua
+++ lua/plugins/Copilot.lua
@@ -20,51 +20,30 @@ return {
 			fix_pairs = true,
 		},
 	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		opts = {
-			show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
-			debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-			disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
-			language = "Spanish", -- Copilot answer language settings when using default prompts. Default language is English.
-			-- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
-			-- temperature = 0.1,
-		},
-		build = function()
-			vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-		end,
-		event = "VeryLazy",
-		keys = {
-			{ "<leader>ccb", ":CopilotChatBuffer ", desc = "CopilotChat - Buffer Actual" },
-			{ "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explica el código" },
-			{ "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
-			{
-				"<leader>ccT",
-				"<cmd>CopilotChatVsplitToggle<cr>",
-				desc = "CopilotChat - Toggle Vsplit", -- Toggle vertical split
-			},
-			{
-				"<leader>ccv",
-				":CopilotChatVisual ",
-				mode = "x",
-				desc = "CopilotChat - Open in vertical split",
-			},
-			{
-				"<leader>ccx",
-				":CopilotChatInPlace<cr>",
-				mode = "x",
-				desc = "CopilotChat - Run in-place code",
-			},
-			{
-				"<leader>ccf",
-				"<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
-				desc = "CopilotChat - Fix diagnostic",
-			},
-			{
-				"<leader>ccr",
-				"<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
-				desc = "CopilotChat - Reset chat history and clear buffer",
-			},
-		},
-	},
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    event = "VeryLazy",
+    branch = "canary",
+    opts = {
+      show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+      language = "Spanish", -- Copilot answer language settings when using default prompts. Default language is English.
+      -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
+      -- temperature = 0.1,
+    },
+    build = function()
+      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    end,
+    keys = {
+      { "<leader>cct", ":CopilotChatToggle<cr>", desc = "Toggle CopilotChat window" },
+      { "<leader>cce", ":CopilotChatExplain<cr>", desc = "Explain selected code" },
+      { "<leader>ccT", "<cmd>CopilotChatTests<cr>", desc = "Generate tests for selected code" },
+      { "<leader>ccf", "<cmd>CopilotChatFix<cr>", desc = "Fix selected selected code" },
+      { "<leader>ccF", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "Assist with current diagnostic" },
+      { "<leader>cco", "<cmd>CopilotChatOptimize<cr>", desc = "Optimize selected code" },
+      { "<leader>ccd", "<cmd>CopilotChatDocs<cr>", desc = "Document selected code" },
+      { "<leader>ccc", "<cmd>CopilotChatCommit<cr>", desc = "Write commit message" },
+    },
+  },
 }
