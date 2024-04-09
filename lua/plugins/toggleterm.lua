7288990d4a22d7e09aee090efc21cf616514aeed
Jorge Meneses <jorgejhms@gmail.com>
2024-04-09 17:08:46 -0500

    Cambiando plugin lazygit por toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,29 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		local Terminal = require("toggleterm.terminal").Terminal
+
+		-- Configura Lazygit
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			direction = "float",
+			float_opts = {
+				border = "curved",
+				title_pos = "center",
+			},
+		})
+
+		function LazygitToggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>gg",
+			"<cmd>lua LazygitToggle()<CR>",
+			{ desc = "Open Lazygit", noremap = true, silent = true }
+		)
+	end,
+}
