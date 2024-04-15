f6008e4ec2bae0fe7ed02110b0f34bafc7260123
Jorge Meneses <jorgejhms@gmail.com>
2024-04-15 16:50:41 -0500

    agregando harpoon

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -167,3 +167,28 @@ require("virt-column").setup({
 -- R language
 add("R-nvim/R.nvim")
 add("R-nvim/cmp-r")
+
+-- Harpoon
+add({
+  source = "ThePrimeagen/harpoon",
+  checkout = "harpoon2",
+  depends = { "nvim-lua/plenary.nvim" },
+})
+
+local harpoon = require("harpoon")
+harpoon:setup()
+
+-- keymap
+vim.keymap.set("n", "<leader>H", function()
+  harpoon:list():add()
+end, { desc = "Agregar archivo (Harpoon)" })
+vim.keymap.set("n", "<leader>h", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Toggle quick menu (Harpoon)" })
+
+-- loop 1 to 5 to set leader numberr to select file
+for i = 1, 5 do
+  vim.keymap.set("n", "<leader>" .. i, function()
+    harpoon:list():select(i)
+  end, { desc = "Abrir archivo " .. i .. " (Harpoon)" })
+end
