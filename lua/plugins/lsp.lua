0f0984fdb8d4916bf3efb4632ddbb21ea29451ac
Jorge Meneses <jorgejhms@gmail.com>
2024-04-17 13:02:54 -0500

    Agregando soporte Astro

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ local lspconfig = require("lspconfig")
 lspconfig.dockerls.setup({})
 lspconfig.docker_compose_language_service.setup({})
 lspconfig.marksman.setup({})
+lspconfig.astro.setup({})
 
 lspconfig.jsonls.setup({ -- [[ JSON config ]]
   -- lazy-load schemastore when needed
