88c07aa6aaf6cc88e8777d510edcd872ad96d417
Jorge Meneses <jorgejhms@gmail.com>
2024-04-23 21:15:59 -0500

    eliminado maps incluidos en mini.bracketed

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -10,8 +10,6 @@ vim.opt.hlsearch = true
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 map("n", "<leader>de", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 map("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
