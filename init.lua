2077b17b4a3ae4830340f64b96e786defad574b7
Jorge Meneses <jorgejhms@gmail.com>
2024-05-07 16:18:43 -0500

    agregando noice

--- init.lua
+++ init.lua
@@ -46,7 +46,7 @@ local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 -- stylua: ignore start
 now(function() require("config.options") end) -- Opciones generales
 now(function() require("config.maps") end) -- Keymaps
-now(function() require("plugins.mini-notify") end) -- Notificaciones
+-- now(function() require("plugins.mini-notify") end) -- Notificaciones
 now(function() require("mini.starter").setup() end) -- Pantalla inicial
 now(function() require("mini.sessions").setup() end) -- Gestión de sesiones
 now(function() require("plugins.colors") end) -- Temas de colores
@@ -345,6 +345,43 @@ later(function()
         "lazyterm",
       },
     },
+  })
+end)
+
+-- Noice
+later(function()
+  add({ source = "folke/noice.nvim", depends = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" } })
+  require("noice").setup({
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+  })
+  require("notify").setup({
+    fps = 60,
+    stages = "fade",
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
   })
 end)
 
