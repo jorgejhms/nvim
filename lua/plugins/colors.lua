ae0f9ed567582c3faa086e05ec75d3b8298186ec
Jorge Meneses <jorgejhms@gmail.com>
2024-05-13 13:27:56 -0500

    retirando colores no compotables

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -8,20 +8,22 @@ add("binhtran432k/dracula.nvim")
 add("sainnhe/edge")
 add("navarasu/onedark.nvim")
 add("EdenEast/nightfox.nvim")
-add("morhetz/gruvbox")
-add("Shatur/neovim-ayu")
-add("oxfist/night-owl.nvim")
-add("rafamadriz/neon")
+add("sainnhe/gruvbox-material")
+-- add("Shatur/neovim-ayu")
+-- add("oxfist/night-owl.nvim")
+-- add("rafamadriz/neon")
 add("sainnhe/sonokai")
 add("tanvirtin/monokai.nvim")
 add("projekt0n/github-nvim-theme")
-add("nyngwang/nvimgelion")
-add("mhartington/oceanic-next")
+-- add("nyngwang/nvimgelion")
+-- add("mhartington/oceanic-next")
 add("rebelot/kanagawa.nvim")
 add("shaunsingh/nord.nvim")
-add("NTBBloodbath/doom-one.nvim")
-add("sontungexpt/witch")
+-- add("NTBBloodbath/doom-one.nvim")
+-- add("sontungexpt/witch")
 add("bluz71/vim-moonfly-colors")
+-- add("rose-pine/neovim")
+add("scottmckendry/cyberdream.nvim")
 
 -- Opciones adicionales dracula
 require("dracula").setup({
@@ -31,6 +33,26 @@ require("dracula").setup({
     sidebars = "dark",
   },
   floats = "transparent",
+})
+
+-- cyberdream
+require("cyberdream").setup({
+  -- Recommended - see "Configuring" below for more config options
+  transparent = true,
+  italic_comments = true,
+  hide_fillchars = true,
+  borderless_telescope = true,
+  terminal_colors = true,
+})
+
+-- catppuccin
+require("catppuccin").setup({
+  integrations = {
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+  },
 })
 
 -- Tema por defecto
