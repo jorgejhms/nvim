6cfebe41dd569d8ef98dab8f2baed2c66f8ee564
Jorge Meneses <jorgejhms@gmail.com>
2024-05-14 14:59:57 -0500

    agregando selección incremental

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -23,6 +23,15 @@ require("nvim-treesitter.configs").setup({
     "tsx",
     "yaml",
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
   -- Autoinstall languages that are not installed
   auto_install = true,
   highlight = {
