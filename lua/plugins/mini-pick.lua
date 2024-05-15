ead26919718d00bf623457c8ce0eb346115f27d2
Jorge Meneses <jorgejhms@gmail.com>
2024-05-15 11:48:28 -0500

    agregando defaults maps en spellsuggest

--- lua/plugins/mini-pick.lua
+++ lua/plugins/mini-pick.lua
@@ -58,6 +58,42 @@ end
 local spellsuggest = function()
   local local_opts = { n_suggestions = 5 } -- You can adjust the number of suggestions as needed
   local opts = {
+    mappings = {
+      caret_left = "<Left>",
+      caret_right = "<Right>",
+
+      choose = "<CR>",
+      choose_in_split = "<C-s>",
+      choose_in_tabpage = "<C-t>",
+      choose_in_vsplit = "<C-v>",
+      choose_marked = "<M-CR>",
+
+      delete_char = "<BS>",
+      delete_char_right = "<Del>",
+      delete_left = "<C-u>",
+      delete_word = "<C-w>",
+
+      mark = "<C-x>",
+      mark_all = "<C-a>",
+
+      move_down = "<C-n>",
+      move_start = "<C-g>",
+      move_up = "<C-p>",
+
+      paste = "<C-r>",
+
+      refine = "<C-Space>",
+      refine_marked = "<M-Space>",
+
+      scroll_down = "<C-f>",
+      scroll_left = "<C-h>",
+      scroll_right = "<C-l>",
+      scroll_up = "<C-b>",
+
+      stop = "<Esc>",
+
+      toggle_info = "<S-Tab>",
+    },
     window = {
       config = {
         relative = "cursor",
