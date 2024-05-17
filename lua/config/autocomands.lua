0c09c2df2992de4b22e0c18fd5534d3ee782a72a
Jorge Meneses <jorgejhms@gmail.com>
2024-05-17 17:15:31 -0500

    feat: Oculta mini.tabline si solo hay un buffer abierto

--- lua/config/autocomands.lua
+++ lua/config/autocomands.lua
@@ -29,3 +29,19 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
     vim.cmd("tabnext " .. current_tab)
   end,
 })
+
+-- Oculta mini.tabline si solo hay un buffer abierto
+-- https://github.com/echasnovski/mini.nvim/issues/433
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = vim.schedule_wrap(function()
+    local n_listed_bufs = 0
+    for _, buf_id in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.fn.buflisted(buf_id) == 1 then
+        n_listed_bufs = n_listed_bufs + 1
+      end
+    end
+
+    vim.o.showtabline = n_listed_bufs > 1 and 2 or 0
+  end),
+  desc = "Update tabline based on the number of listed buffers",
+})
