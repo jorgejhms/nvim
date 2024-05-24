887b46a0430add979bf06bbfb7f8b551c3de6852
Jorge Meneses <jorgejhms@gmail.com>
2024-05-24 13:19:16 -0500

    eliminando comentarios

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -8,8 +8,6 @@ return {
       debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
       disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
       language = "Spanish", -- Copilot answer language settings when using default prompts. Default language is English.
-      -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
-      -- temperature = 0.1,
     },
     build = function()
       vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
