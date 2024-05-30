f2fdc4cb29a5b8d0ad3e6e992fcfdfb95d1669d1
Jorge Meneses <jorgejhms@gmail.com>
2024-05-29 22:34:32 -0500

    cambiando default a telescope

--- lua/plugins/mini-pick.lua
+++ lua/plugins/mini-pick.lua
@@ -16,9 +16,7 @@ end
 
 -- MiniPick Colorscheme Picker
 local set_colorscheme = function(name)
-  pcall(function()
-    vim.cmd("Colorscheme " .. name)
-  end)
+  pcall(function() vim.cmd("Colorscheme " .. name) end)
 end
 local pick_colorscheme = function()
   local init_scheme = vim.g.colors_name
@@ -36,19 +34,13 @@ local pick_colorscheme = function()
         char = "<Tab>",
         func = function()
           local item = MiniPick.get_picker_matches()
-          if item and item.current then
-            pcall(function()
-              vim.cmd("colorscheme " .. item.current)
-            end)
-          end
+          if item and item.current then pcall(function() vim.cmd("colorscheme " .. item.current) end) end
         end,
       },
     },
     window = { config = win_config },
   })
-  if new_scheme == nil then
-    set_colorscheme(init_scheme)
-  end
+  if new_scheme == nil then set_colorscheme(init_scheme) end
 end
 
 -- Mini picker spellsuggest
@@ -92,19 +84,19 @@ local spellsuggest = function()
 end
 
 -- Keymaps
-map("n", "<Leader><Leader>", "<cmd>Pick files<CR>", { desc = "[P]ick [F]iles" })
-map("n", "<Leader>sf", "<cmd>Pick files<CR>", { desc = "[S]earch [F]iles" })
-map("n", "<Leader>sk", "<cmd>Pick keymaps<CR>", { desc = "[P]ick [K]eymaps" })
-map("n", "<Leader>sh", "<cmd>Pick help<CR>", { desc = "[S]earch [H]elp" })
-map("n", "<Leader>sb", "<cmd>Pick buffers<CR>", { desc = "[S]earch [B]uffers" })
-map("n", "<Leader>sc", "<cmd>Pick commands<CR>", { desc = "[S]earch [C]ommands" })
-map("n", "<Leader>sm", "<cmd>Pick marks<CR>", { desc = "[S]earch [M]arks" })
-map("n", "<Leader>so", "<cmd>Pick options<CR>", { desc = "[S]earch [O]ptions" })
-map("n", "<Leader>s.", "<cmd>Pick oldfiles<CR>", { desc = "[S]earch [R]ecent files" })
-map("n", "<Leader>sd", "<cmd>Pick diagnostic<CR>", { desc = "[S]earch [D]iagnostics" })
-map("n", "<Leader>sr", "<cmd>Pick resume<CR>", { desc = "[S]earch [R]esume" })
-map("n", "<Leader>sg", "<cmd>Pick grep_live<CR>", { desc = "[S]earch [G]rep" })
+-- map("n", "<Leader><Leader>", "<cmd>Pick files<CR>", { desc = "[P]ick [F]iles" })
+-- map("n", "<Leader>sf", "<cmd>Pick files<CR>", { desc = "[S]earch [F]iles" })
+-- map("n", "<Leader>sk", "<cmd>Pick keymaps<CR>", { desc = "[P]ick [K]eymaps" })
+-- map("n", "<Leader>sh", "<cmd>Pick help<CR>", { desc = "[S]earch [H]elp" })
+-- map("n", "<Leader>sb", "<cmd>Pick buffers<CR>", { desc = "[S]earch [B]uffers" })
+-- map("n", "<Leader>sc", "<cmd>Pick commands<CR>", { desc = "[S]earch [C]ommands" })
+-- map("n", "<Leader>sm", "<cmd>Pick marks<CR>", { desc = "[S]earch [M]arks" })
+-- map("n", "<Leader>so", "<cmd>Pick options<CR>", { desc = "[S]earch [O]ptions" })
+-- map("n", "<Leader>s.", "<cmd>Pick oldfiles<CR>", { desc = "[S]earch [R]ecent files" })
+-- map("n", "<Leader>sd", "<cmd>Pick diagnostic<CR>", { desc = "[S]earch [D]iagnostics" })
+-- map("n", "<Leader>sr", "<cmd>Pick resume<CR>", { desc = "[S]earch [R]esume" })
+-- map("n", "<Leader>sg", "<cmd>Pick grep_live<CR>", { desc = "[S]earch [G]rep" })
 map("n", "z=", spellsuggest, { desc = "Abre las sugerencias ortográficas" })
-map("n", "<Leader>/", "<cmd>Pick buf_lines scope='current'<CR>", { desc = "[/] Fuzzily search in current buffer" })
-map("n", "<Leader>s/", "<cmd>Pick buf_lines<CR>", { desc = "[S]earch [/] in Open Files" })
-map("n", "<leader>fc", pick_colorscheme, { noremap = true, silent = true, desc = "Change Colorscheme" })
+-- map("n", "<Leader>/", "<cmd>Pick buf_lines scope='current'<CR>", { desc = "[/] Fuzzily search in current buffer" })
+-- map("n", "<Leader>s/", "<cmd>Pick buf_lines<CR>", { desc = "[S]earch [/] in Open Files" })
+-- map("n", "<leader>fc", pick_colorscheme, { noremap = true, silent = true, desc = "Change Colorscheme" })
