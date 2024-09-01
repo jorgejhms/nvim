2b5137c315d91c7bdcaf54ae82788a47e4ed655e
Jorge Meneses <jorgejhms@gmail.com>
2024-09-01 18:03:00 -0500

    moviendo config deps

--- init.lua
+++ init.lua
@@ -16,8 +16,22 @@
 --]]
 
 -- Carga e inicialización de MiniDeps
-local deps = pcall(require, "config.deps")
-if not deps then print("No se pudo instalar 'MiniDeps'") end
+-- Clona mini.nvim si no está instalado
+local path_package = vim.fn.stdpath("data") .. "/site/"
+local mini_path = path_package .. "pack/deps/start/mini.nvim"
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/echasnovski/mini.nvim",
+    mini_path,
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd("packadd mini.nvim | helptags ALL")
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
 
 -- Agrega 'helpers'
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
